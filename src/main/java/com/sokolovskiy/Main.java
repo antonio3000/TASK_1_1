ff84ed046cee00e48aa82a642a5259ba70a4460c
antonio3000 <tohasokol28@gmail.com>
2018-10-15 23:22:12 +0300

    origin

--- src/main/java/com/sokolovskiy/Main.java
+++ src/main/java/com/sokolovskiy/Main.java
@@ -1,4 +1,7 @@
 package com.sokolovskiy;
 
-public class Main {
+public class Main {    public static void
+    mymain(String[] args){
+        System.out.println("Соколовский Антон, ЛПВФП 11-Ю");
+    }
 }
